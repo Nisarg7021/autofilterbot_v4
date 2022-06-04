be6bb49a58a441481b1225bb9a707837fefdfd1f
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-06-04 17:52:06 +0530

    Update url_shortner.py

--- plugins/Don/url_shortner.py
+++ plugins/Don/url_shortner.py
@@ -8,7 +8,7 @@ from pyshorteners import Shortener
 BITLY_API = os.environ.get("BITLY_API", "8df1df8c23f719e5cf97788cc2d40321ea30092b")
 CUTTLY_API = os.environ.get("CUTTLY_API", "4930f225215bdb46c6be5d969700a5a9fd2ff")
 SHORTCM_API = os.environ.get("SHORTCM_API", "pk_...NIZv")
-GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9007a7")
+GPLINKS_API = os.environ.get("GPLINKS_API", "681bf6ed2bdc16d93cdc6842be62a1baf6364e82")
 
 reply_markup = InlineKeyboardMarkup(
         [[
