821580122b086800465536e4c674653eccf1c7ac
Nisarg7021 <95787542+Nisarg7021@users.noreply.github.com>
2022-06-26 01:46:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -176,7 +176,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{📩DOWNLOAD(DOWNLOAD)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_text(file.text_text)}] {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
